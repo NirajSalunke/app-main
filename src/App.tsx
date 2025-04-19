4ff360d5d271ac38ea5df8e158dfd56097353f39
NirajSalunke <154681227+NirajSalunke@users.noreply.github.com>
2025-04-19 13:51:13 +0530

    chota mota change

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 // import DashBoard from "./pages/dashboard";
 import Home from "./pages/Home";
 import DashBoard from "./pages/DashBoard";
